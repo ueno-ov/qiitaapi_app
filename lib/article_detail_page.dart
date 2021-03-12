da2f615528b1a41dec96b1a50e78e21ee5d9c1af
ueno-ov <12321klj@gmail.com>
2021-03-12 18:53:14 +0900

    記事詳細ページの作成

---
+++ lib/article_detail_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'article.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+
+
+
+class ArticleDetailPage extends StatelessWidget {
+  final Article article;
+
+  ArticleDetailPage({Key key, this.article}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: MaterialApp(
+        title: 'Fetch Data Example',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: WebView(
+              initialUrl: article.url,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
